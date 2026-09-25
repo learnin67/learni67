f035dd05665eb9d11df7fd88905921627c400ded
learnin67 <khanhnn.2510501@usth.edu.vn>
2026-09-25 20:59:44 +0700

    Update output.py

--- output.py
+++ output.py
@@ -1,1 +1,29 @@
+def new_screen(title):
+    global y
+    screen.clear()
+    screen.border(0)
+    screen.addstr(0, 2, title)        
+    y = 2
+
+def show(text):
+    global y
+    try:
+        screen.addstr(y, 2, text)
+    except curses.error:             
+        pass
+    y = y + 1
+    screen.refresh()
+
+def ask(prompt):
+    global y
+    screen.addstr(y, 2, prompt)
+    screen.refresh()
+    answer = screen.getstr(y, 2 + len(prompt)).decode()
+    y = y + 1
+    return answer
+
+def pause():
+    show("")
+    show("Press any key to continue...")
+    screen.getch()
 
